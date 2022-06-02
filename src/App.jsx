738386a546955dfad5f55eea9a6681a18fde7528
LUCIO Notebook <luciononato@hotmail.com>
2022-06-02 19:17:33 -0300

    Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { Header } from "./components/Header";
 
+import styles from './App.module.css'
 import './global.css'
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 
-  return (
-      <>
-      <Header/>
-      </>
-    )
+	return (
+		<>
+			<Header />
+
+			<div className={styles.wrapper}>
+				<Sidebar/>
+
+				<main>
+					<Post
+						author="Lúcio"
+						content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima et laudantium omnis aut rem beatae maxime tempora vel totam eos ea est ratione voluptas, hic, officiis ipsam dolores enim consequuntur"
+					/>
+				</main>
+			</div>
+		</>
+	)
 }
