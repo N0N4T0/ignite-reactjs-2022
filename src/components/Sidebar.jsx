462514ba66b80fc955b980188c4be23fb9ec636a
LUCIO Notebook <luciononato@hotmail.com>
2022-06-03 21:31:11 -0300

    finalizando sideBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
@@ -9,12 +10,15 @@ export function Sidebar(){
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/39486464?v=4" alt="" />
+
                 <strong>Lúcio</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href='#'>
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
