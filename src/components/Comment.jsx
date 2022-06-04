2be3290443264f1512eb7bec0544b187f775a5cd
LUCIO Notebook <luciononato@hotmail.com>
2022-06-04 20:52:20 -0300

    componente comentário

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/39486464?v=4" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Lúcio Nonato</strong>
+
+                            <time
+                                title='11 de Maio às 08:13h' 
+                                dateTime='2022-06-03 08:13:40'
+                            >
+                                Cerca de 1h atrás
+                            </time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
