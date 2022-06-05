465e7d3f5be54220c9c8165e743bee900232cfc8
LUCIO Notebook <luciononato@hotmail.com>
2022-06-05 19:00:13 -0300

    rest operator on props

--- 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
@@ -1,17 +1,19 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css'
 
-interface AvatarProps {
+// FAzendo essa extensão eu não preciso passar na interface o que já existe 
+// por padrão 
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
     hasBorder?: boolean;
-    src: string;
-    alt?: string;
 }
 
-export function Avatar({src, hasBorder = true, alt}: AvatarProps){
+// Usa-se o pread operator nas props para pegar tudo que quiser
+//  e evitar ficar adicionando mais tipagens
+export function Avatar({hasBorder = true, ...props}: AvatarProps){
     return(
         <img 
             className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-            src={src} 
-            alt={alt}
+            {...props}
         />
     )
 }
