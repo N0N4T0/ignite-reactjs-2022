9884f375c5f1fc6d87ac81413a031fc39a8f5cfa
LUCIO Notebook <luciononato@hotmail.com>
2022-08-11 07:35:19 -0300

    feat: add title page when cycle start, fix bug to clean interval with cleanup useEffect

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -57,6 +57,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -68,12 +69,18 @@ export function Home() {
   // Sempre que utilizamos uma variável externa no useEffect precisamos incluir
   // essa variável como dependência
   React.useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+
+      return () => {
+        clearInterval(interval)
+      }
     }
   }, [activeCycle])
 
@@ -85,6 +92,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  React.useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
