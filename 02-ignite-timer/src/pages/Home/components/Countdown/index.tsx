dc28adacb16ec2091f7f30b4897e95b75aed580d
LUCIO Notebook <luciononato@hotmail.com>
2022-09-01 23:31:48 -0300

    feat: add context

--- 02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
+++ 02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,69 @@
+import { differenceInSeconds } from 'date-fns'
+import React, { useContext } from 'react'
+import { CyclesContext } from '../..'
 import { CountContainer, Separator } from './styles'
 
 export function Countdown() {
+  const {
+    activeCycle,
+    activeCycleId,
+    amountSecondsPassed,
+    markCurrentCycleAsFinished,
+    setSecondsPassed,
+  } = useContext(CyclesContext)
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+
+  // COmo o setInterval não é preciso, a lógica será comparar a data atual com
+  // a data qeu salvei no starDate e ver quantos segundos se passaram
+  // Sempre que utilizamos uma variável externa no useEffect precisamos incluir
+  // essa variável como dependência
+  React.useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate,
+        )
+
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()
+
+          setSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setSecondsPassed(secondsDifference)
+        }
+      }, 1000)
+
+      return () => {
+        clearInterval(interval)
+      }
+    }
+  }, [
+    activeCycle,
+    totalSeconds,
+    activeCycleId,
+    markCurrentCycleAsFinished,
+    setSecondsPassed,
+  ])
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  React.useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
+
   return (
     <CountContainer>
       <span>{minutes[0]}</span>
