a0299ed18d2bcceccaf6534fd10e090d7d49907e
Lucio <luciononato@hotmail.com>
2022-11-06 17:57:09 -0300

    feat: format values

--- 03-dt-money/src/components/Summary/index.tsx
+++ 03-dt-money/src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { useContext } from "react";
 import { useTheme } from "styled-components";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 import { SummaryCard, SummaryContainer } from "./styles";
 
 export function Summary() {
@@ -36,7 +37,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={colors["green-300"]} />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -45,7 +46,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={colors["red-300"]} />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -54,7 +55,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={colors.white} />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
